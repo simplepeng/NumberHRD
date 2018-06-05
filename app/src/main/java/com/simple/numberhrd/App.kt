351fa3fa857d40d09892b5bc96a5c1f0412c06b4
simplepeng <383559698@qq.com>
2018-06-05 15:14:29 +0800

    add kapt,icon

--- app/src/main/java/com/simple/numberhrd/App.kt
+++ app/src/main/java/com/simple/numberhrd/App.kt
@@ -9,6 +9,10 @@ import com.simple.numberhrd.db.NumberHRD_DB
  */
 class App : Application() {
 
+    companion object {
+        const val adMob_id = "ca-app-pub-2171500527855297~3639703247"
+    }
+
     var db: NumberHRD_DB? = null
 
     override fun onCreate() {
